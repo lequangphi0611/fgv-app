963d7726d1372f2163c8fd875e72ac1601be21d0
le quang phi <lequangphi0611@gmail.com>
2023-05-17 13:25:33 +0700

    Init - Styled Components - add global styles

---
+++ src/app/global-style.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    p {
+      color: red;
+    }
+`;
